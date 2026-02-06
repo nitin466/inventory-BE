4dc22020d2cb77280f9ff1be7fdb1d94edbadab9
Nitin Kumar <nitin466@gmail.com>
2026-02-06 12:47:54 +0530

    port 0

--- src/server.js
+++ src/server.js
@@ -1,11 +1,10 @@
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
 dotenv.config();
-
 
 import app from './app.js';
 
-const PORT = process.env.PORT ?? 3000;
+const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`🚀 Server listening on port ${PORT}`);
 });
